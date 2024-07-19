1a9cc3800b1cb801b179e0c7f670e4a47794bcae
Luke Batchelor <batchelor.luke@gmail.com>
2024-07-19 16:29:13 +0800

    Adds tailwind and shadcn components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from '@/components/ThemeProvider.tsx'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
