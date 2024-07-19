8649c1760b557c97df78d148ae56b8035fe778b7
Luke Batchelor <batchelor.luke@gmail.com>
2024-07-19 16:33:00 +0800

    Runs prettier over repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import { useState } from 'react';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
-import { Button } from './components/ui/button'
+import viteLogo from '/vite.svg';
+import reactLogo from './assets/react.svg';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,25 +18,21 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-3xl font-bold underline text-red-700'>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline text-red-700">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       <Button>Click me</Button>
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { ThemeProvider } from '@/components/ThemeProvider.tsx'
-import App from './App.tsx'
-import './index.css'
+import { ThemeProvider } from '@/components/ThemeProvider.tsx';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <App />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
